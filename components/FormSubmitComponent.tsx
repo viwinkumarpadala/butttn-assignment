ff2c81b7dbaebcff74b705d62280454d53db8730
viwinkumarpadala <viwinkumar.p20@iiits.in>
2024-03-08 09:12:09 +0530

    changed a form element

--- components/FormSubmitComponent.tsx
+++ components/FormSubmitComponent.tsx
@@ -68,7 +68,7 @@ function FormSubmitComponent({ formUrl, content }: { content: FormElementInstanc
       <div className="flex justify-center w-full h-full items-center p-8">
         <div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border shadow-xl shadow-purple-700 rounded">
           <h1 className="text-2xl font-bold">Form submitted!!!</h1>
-          <p className="text-muted-foreground">Thank you for submitting the form, please close this page now.</p>
+          <p className="text-muted-foreground">Thank you for submitting the form!</p>
         </div>
       </div>
     );
